22eb12cd2734d6ba87437b053706f68350818f76
Hasan50 <tapantor24@gmail.com>
2019-06-30 18:19:21 +0600

    js

---
+++ src/assets/js/eshop.js
@@ -0,0 +1,14 @@
+﻿/*==========Scroll Top==========*/
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 1) {
+        $('.scroll-top-wrapper').addClass("show");
+    }
+    else {
+        $('.scroll-top-wrapper').removeClass("show");
+    }
+});
+$(".scroll-top-wrapper").on("click", function () {
+    $("html, body").animate({ scrollTop: 0 }, 600);
+    return false;
+});
+/*==========Scroll Top==========*/

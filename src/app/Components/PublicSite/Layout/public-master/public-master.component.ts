c02b7d1d2e443a658afe3c410015ac01a69e6b14
Hasan50 <tapantor24@gmail.com>
2019-07-07 22:52:43 +0600

    Update public-master.component.ts

--- src/app/Components/PublicSite/Layout/public-master/public-master.component.ts
+++ src/app/Components/PublicSite/Layout/public-master/public-master.component.ts
@@ -6,7 +6,6 @@ import * as $ from "jquery";
 @Component({
   selector: 'app-public-master',
   templateUrl: './public-master.component.html',
-  styleUrls: ['./public-master.component.scss']
   styleUrls: ['./public-master.component.scss'],
 })
 export class PublicMasterComponent implements OnInit {
